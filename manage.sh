921924ce3340563d66e27d11bff29988d29b2da7
Lv Zheng <lv.zheng.2015@gmail.com>
2016-04-06 10:47:33 -0400

    rewrite the whole project thoroughly
    Remove `jekyll` and `git` from container.
    Remove `manage.sh`, use `CMD` to directly invoke `tunasync` and `nginx`.
    Split the `Dockerfile` into two. One for `tunasync`, the other for `nginx`.
    Host server is responsible to prepare (build) the static pages. The static pages are stored in docker volumes.
    Change `Dockerfile` into style recommended by the official doc.

--- manage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Starting nginx"
-service nginx start
-echo $?
-echo "Starting jekyll"
-echo $?
-/usr/local/bin/jekyll serve -s /home/mirror-web -t /home/mirror-web/_site --watch &
-echo "Starting tunasync"
-python2 /home/tunasync/tunasync.py -c /home/tunasync/tunasync.conf
