1eceed5c68c85ce7ba9953119b19409cede57a28
Zhenbo Li <litimetal@gmail.com>
2016-05-15 19:40:00 +0800

    Fix typo

--- nginx/copykey.sh
+++ nginx/copykey.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 site_="mirrors.sjtug.org"
-copy -u "$HOME/.acme.sh/$site_/fullchain.cer" cert.cer
-copy -u "$HOME/.acme.sh/$site_/$site_.key" key.key
+cp -u "$HOME/.acme.sh/$site_/fullchain.cer" cert.cer
+cp -u "$HOME/.acme.sh/$site_/$site_.key" key.key
 
