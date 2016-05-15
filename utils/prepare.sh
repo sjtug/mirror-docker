7f0e30abba5733a87990b3a189724cf9b07638ac
Zhenbo Li <litimetal@gmail.com>
2016-05-15 19:37:47 +0800

    prepare.sh can be executed in any folder.

--- utils/prepare.sh
+++ utils/prepare.sh
@@ -1,19 +1,34 @@
 #!/bin/sh
+
+ROOT=`dirname $(pwd)/"$0"`
+cd $ROOT
+while true; do
+    ROOT=`pwd`
+    if [ $ROOT = '/' ]; then echo "Failed to find right path!"; exit 1; fi
+    if [ -f LICENSE ] && [ -d utils ] ; then
+        echo "Found path $ROOT"
+        break
+    fi
+    cd ..
+done
+cd $ROOT
+ls
+
 echo "Issuing Cert with acme.sh"
-source acquire_cert.sh
+source ./acquire_cert.sh
 
 echo "Building TunaSync"
 cd tunasync
-bash build.sh
+bash ./build.sh
 cd ..
 
 echo "Building Nginx"
 cd nginx
-bash build.sh
+bash ./build.sh
 cd ..
 
 echo "Building Jekyll"
 cd jekyll
-bash build.sh
+bash ./build.sh
 cd ..
 
