70fcb1a082610bdf8136fa43e5259dd2d1b74663
root <root@mirrors2.sjtug.org>
2018-03-08 21:18:47 +0800

    fix rsync.sh

--- lug/worker-script/rsync.sh
+++ lug/worker-script/rsync.sh
@@ -2,5 +2,6 @@
 
 if [ "$LUG_exclude_hidden" ]; then
 	exclude_hidden_flags="--exclude=.*"
+fi
 
-rsync -aHvh --no-o --no-g --stats --delete --delete-delay --safe-links --partial-dir=.rsync-partial --timeout=120 --conntimeout=120 "$exclude_hidden_flags" "$LUG_source" "$LUG_path"
+rsync -aHvh --no-o --no-g --stats --delete --delete-delay --safe-links --partial-dir=.rsync-partial --timeout=120 --contimeout=120 $exclude_hidden_flags "$LUG_source" "$LUG_path"
