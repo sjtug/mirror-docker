ad69d29bd2e7f32bf0f850feeda90c18d24c92cb
Vic Luo <vicluo96@gmail.com>
2018-03-11 23:20:15 +0800

    rsync: add exclude_vanish switch

--- lug/worker-script/rsync.sh
+++ lug/worker-script/rsync.sh
@@ -4,4 +4,27 @@ if [ "$LUG_exclude_hidden" ]; then
 	exclude_hidden_flags="--exclude=.*"
 fi
 
-rsync -aHvh --no-o --no-g --stats --delete --delete-delay --safe-links --exclude '.~tmp~/' --partial-dir=.rsync-partial --timeout=120 --contimeout=120 $exclude_hidden_flags "$LUG_source" "$LUG_path"
+if [ "LUG_ignore_vanish" ]; then
+	IGNOREEXIT=24
+	IGNOREOUT='^(file has vanished: |rsync warning: some files vanished before they could be transferred)'
+fi
+
+tmp_stderr=$(mktemp "/tmp/rsync-$LUG_name.XXX")
+
+rsync -aHvh --no-o --no-g --stats --delete --delete-delay --safe-links --exclude '.~tmp~/' --partial-dir=.rsync-partial --timeout=120 --contimeout=120 $exclude_hidden_flags "$LUG_source" "$LUG_path" 2> "$tmp_stderr"
+retcode="$?"
+
+cat "$tmp_stderr" >&2
+
+if [ "LUG_ignore_vanish" ]; then
+	if [ "$retcode" -eq "$IGNOREEXIT" ]; then
+		if egrep "$IGNOREOUT" "$tmp_stderr"; then
+			retcode=0
+		fi
+	fi
+fi
+
+rm -f "$tmp_stderr"
+
+exit "$retcode"
+
