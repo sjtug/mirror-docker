415eb299fdb83549074e657cd8f3e7a2a4d889f3
Vic Luo <vicluo96@gmail.com>
2018-05-17 16:45:54 +0800

    lug: worker-script: googlestorage: fix variable

--- lug/worker-script/googlestorage.sh
+++ lug/worker-script/googlestorage.sh
@@ -6,5 +6,5 @@
 # project_id: The project name where you create a new key
 # source: source googlestorage path. Example: gs://flutter_intra
 
-mkdir -p "LUG_path"
-env "GS_ACCESS_KEY_ID=$LUG_access_key_id" "GS_ACCESS_SECRET_KEY=$LUG_access_secret_key" ./google-cloud-sdk/bin/gsutil  -o "GSUtil:default_project_id=$LUG_project_id" -m rsync -d -r "LUG_source" "$LUG_path"
+mkdir -p "$LUG_path"
+env "GS_ACCESS_KEY_ID=$LUG_access_key_id" "GS_ACCESS_SECRET_KEY=$LUG_access_secret_key" ./google-cloud-sdk/bin/gsutil  -o "GSUtil:default_project_id=$LUG_project_id" -m rsync -d -r "$LUG_source" "$LUG_path"
