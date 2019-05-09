2c8073e807304c6bf105540bb8d31572ba850811
DCjanus <2015060101010@std.uestc.edu.cn>
2019-05-09 14:59:43 +0800

    fix crates-io.sh(unexpected redirect) (#27)

--- lug/worker-script/crates-io.sh
+++ lug/worker-script/crates-io.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -xe
 /worker-script/git.sh
-cd "$LUG_path" && sed -i 's/"dl": *"https:\/\/crates.io/"dl": "https:\/\/crates-io.mirrors.sjtug.sjtu.edu.cn/' config.json
+pushd "$LUG_path"
+jq 'setpath(["dl"]; "https://mirrors.sjtug.sjtu.edu.cn/static.crates.io/crates/{crate}/{crate}-{version}.crate")' config.json > config.json.temp
+mv config.json.temp config.json
+popd
